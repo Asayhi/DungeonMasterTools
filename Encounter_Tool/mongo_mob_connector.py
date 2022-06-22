16307fc68bb404e794963c492111697773e4bd92
Manuel <manuel.amesberger@stud.th-deg.de>
2022-06-22 21:57:27 +0200

    saving and loading with mongo added

--- Encounter_Tool/mongo_mob_connector.py
+++ Encounter_Tool/mongo_mob_connector.py
@@ -1,5 +1,4 @@
 from unittest import result
-from json5 import load
 from pymongo import MongoClient
 from pprint import pprint
 import encounter_mob as mob
@@ -20,15 +19,15 @@ def save_mob_to_db(monster: mob.Monster):
 def load_mob_from_db(name: str):
     client = MongoClient(port=27017)
     db = client.monsters
-    data = db.find_one({"name": name})
+    data = db.monsters.find_one({"name": name})
     loaded_mob = mob.Monster()
-    loaded_mob.set_misc(data["name"], data["monster_type"], data["size"], data["alignment"], data["version"])
-    combat_data = data["combat_stats"]
-    loaded_mob.combat_stats.set_combat_values(  combat_data["armor_class"], combat_data["armor_class_type"], 
-                                                combat_data["passive_perception"], combat_data["hit_die_count"], 
-                                                combat_data["hit_die_value"], combat_data["hit_points_average"] )
-    loaded_mob.combat_stats.set_attribute_values(   combat_data["str_score"], combat_data["dex_score"],combat_data["con_score"],
-                                                    combat_data["int_score"],combat_data["wis_score"],combat_data["cha_score"])
+    loaded_mob.set_misc(data["name"], data["type"], data["size"], data["alignment"], data["version"])
+
+    loaded_mob.combat_stats.set_combat_values(  data["armor_class"], data["armor_class_type"], 
+                                                data["passive_perception"], data["hit_die_count"], 
+                                                data["hit_die_value"], data["hit_points_average"] )
+    loaded_mob.combat_stats.set_attribute_values(   data["str_score"], data["dex_score"], data["con_score"],
+                                                    data["int_score"], data["wis_score"], data["cha_score"])
     return loaded_mob
 
 
@@ -40,4 +39,5 @@ if __name__ == "__main__":
     test_monster.combat_stats.set_combat_values(0,0,0,0,0,0)
     test_monster.combat_stats.set_attribute_values(20,20,20,20,20,20)
     save_mob_to_db(test_monster)
-    load_mob_from_db("Tester")
+    test_mob = load_mob_from_db("Tester")
+    print("Ended")
